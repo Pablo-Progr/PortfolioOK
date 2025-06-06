8468e7f1abeb9c82b8e939ab5663960c48fe2be6
Pablogd <dominguezpg.99@gmail.com>
2025-06-05 21:10:28 -0300

    cards de proyectos y formulario

--- tp3com2-dominguez-pablo/src/components/MainContact.jsx
+++ tp3com2-dominguez-pablo/src/components/MainContact.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { Form } from 'react-bootstrap'
 import '../css/mainContact.css'
 
 const MainContact = () => {
   return (
-    <div>
-      Main Contact
+    <div className='container d-flex justify-content-center flex-column mt-5 mb-5'>
+      <Form className='form'>
+          <h2 >Contacto</h2>
+      <Form.Group className="mb-3 container" controlId="exampleForm.ControlInput1">
+          <Form.Label className='m-3'>Nombre</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Label className='m-3'>Motivo de Contacto</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Label className='m-3'>Example textarea</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
     </div>
   )
 }
--- tp3com2-dominguez-pablo/src/components/MainProjects.jsx
+++ tp3com2-dominguez-pablo/src/components/MainProjects.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import '../css/mainProjects.css'
+import {Button, Card} from 'react-bootstrap';
+import '../css/mainProjects.css';
+import imgRickAndMorty from '../img/apirickymorty.jpeg'
+import db from '../database/db.json'
+import pedidosya from '../img/pedidosya.jpg';
 
 const MainProjects = () => {
   return (
-    <div>
-      Main Projects
+    <div className='container d-flex justify-content-around flex-wrap mt-5 mb-5'>
+      
+      <Card  className='.cards'>
+      <Card.Img src={imgRickAndMorty} style={{width: '400px'}}/>
+      <Card.Body>
+        <Card.Title>{db.proyectos[0].nombre}</Card.Title>
+        <Card.Text>
+          {db.proyectos[0].descripcion}
+        </Card.Text>
+        <Card.Text>
+          {db.proyectos[0].tecnologias}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='.cards'>
+      <Card.Img src={pedidosya} style={{width: '400px'}} />
+      <Card.Body>
+        <Card.Title>{db.proyectos[1].nombre}</Card.Title>
+        <Card.Text>
+          {db.proyectos[1].descripcion}
+        </Card.Text>
+        <Card.Text>
+          {db.proyectos[1].tecnologias}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+      
     </div>
   )
 }
